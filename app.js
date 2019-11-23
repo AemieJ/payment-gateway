09c1a5fa2b02f8e227992a5220c572280fd7b82d
AemieJ <aemie.j@hotmail.co.uk>
2019-11-23 16:11:33 +0530

    add eslint and prettier to the scripts and json file

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
 const express = require('express');
-const secretKey = require("./api-key.json").secretKey;
+const secretKey = require('./api-key.json').secretKey;
 const stripe = require('stripe')(secretKey);
 const bodyParser = require('body-parser');
-const mongodb = require('mongodb');
 const expressHandleBar = require('express-handlebars');
 
 const app = express();
-const port = 3000; 
+const port = 3000;
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/";
+const url = 'mongodb://localhost:27017/';
 
 // MIDDLEWARE
-app.engine('handlebars' , expressHandleBar({defaultLayout:'main'}));
-app.set('view engine' , 'handlebars');
+app.engine('handlebars', expressHandleBar({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(`${__dirname}/public`));
 
 // Index Route
-app.get('/' , (req , res)=>{
-  res.render('index' , {
-    stripePublicKey: require("./api-key.json").publicKey
+app.get('/', (req, res) => {
+  res.render('index', {
+    stripePublicKey: require('./api-key.json').publicKey
   });
 });
 
 //Charge route
 app.post('/charge', (req, res) => {
   const amount = 250000;
-  
-  dataObject = {
-    email: req.body.stripeEmail , 
-    token: req.body.stripeToken ,
+
+  let dataObject = {
+    email: req.body.stripeEmail,
+    token: req.body.stripeToken,
     amount: 2500
   };
 
   /* CREATING DATABSE WITH MONGODB */
-  MongoClient.connect(url ,  { useUnifiedTopology: true } , (err , db)=>{
+  MongoClient.connect(url, { useUnifiedTopology: true }, (err, db) => {
     if (err) throw err;
-    var database = db.db("payments");
-    database.collection("customers").insertOne(dataObject , (err , res)=>{
-        if (err) throw err;
-        db.close();
+    var database = db.db('payments');
+    database.collection('customers').insertOne(dataObject, (err, _res) => {
+      if (err) throw err;
+      db.close();
     });
 
     /* This will print all the customer in the database 
@@ -52,26 +51,27 @@ app.post('/charge', (req, res) => {
         db.close();
     }); 
     */
-
   });
 
   /* HANDLING PROMISES */
-  stripe.customers.create({
-    email: req.body.stripeEmail,
-    source: req.body.stripeToken
-  })
-  .then(customer =>
-    stripe.charges.create({
-      amount,
-      description: "Demo on Payment",
-      currency: "inr",
-      customer: customer.id
-  }))
-  .then(charge => res.render('charge'))
-  .catch(err => {
-    console.log("Error:", err);
-    res.status(500).send({error: "Purchase Failed"});
-  });
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken
+    })
+    .then(customer =>
+      stripe.charges.create({
+        amount,
+        description: 'Demo on Payment',
+        currency: 'inr',
+        customer: customer.id
+      })
+    )
+    .then(() => res.render('charge'))
+    .catch(err => {
+      console.log('Error:', err);
+      res.status(500).send({ error: 'Purchase Failed' });
+    });
 });
 
 app.listen(port, () => {
